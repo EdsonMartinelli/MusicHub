305cba40e264084d3dd19ab9bea3cbb0011ac793
EdsonMartinelli <martinelli1997@gmail.com>
2023-05-13 22:17:36 -0300

    fix: incorrect load by cache

--- src/client/components/Teste/Video.tsx
+++ src/client/components/Teste/Video.tsx
@@ -1,25 +1,44 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
+import InputRange from "../AudioPlayer/InputRange/InputRange";
 
 export default function Video() {
   const [id, setId] = useState("szVNepsJNkE");
+
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
   const iFrameRef = useRef<HTMLIFrameElement>(null);
 
   useEffect(() => {
+    setIsLoaded(false);
+    iFrameRef.current?.setAttribute("src", iFrameRef.current?.src); // cache iframe
     iFrameRef.current?.addEventListener("load", () => {
       iFrameRef.current?.contentWindow?.postMessage(
         '{"event":"listening"}',
         "*"
       );
+      setIsLoaded(false);
+    });
+
+    iFrameRef.current?.addEventListener("change", () => {
+      console.log("teste2");
     });
   }, []);
 
   useEffect(() => {
     window.addEventListener("message", (event) => {
       const data = JSON.parse(event.data);
+      if (data.event == "initialDelivery" && data.info && data.info?.duration) {
+        setDuration(data.info?.duration);
+        setCurrentTime(0);
+      }
+      if (data.event == "onReady") {
+        setIsLoaded(true);
+      }
       if (data.event == "infoDelivery" && data.info && data.info?.currentTime) {
-        console.log(data.info.currentTime);
+        setCurrentTime(data.info.currentTime);
       }
     });
   }, []);
@@ -30,8 +49,8 @@ export default function Video() {
     iFrameRef.current?.contentWindow?.postMessage(JSON.stringify(t), "*");
   }
 
-  function handleSeekTo() {
-    seekTo([30, true]);
+  function handleSeekTo(e: number) {
+    seekTo([e, true]);
   }
 
   function handleVideo1() {
@@ -45,6 +64,8 @@ export default function Video() {
   return (
     <>
       <iframe
+        className={`${!isLoaded ? "hidden" : ""}`}
+        id={id}
         ref={iFrameRef}
         width="640"
         height="360"
@@ -56,6 +77,20 @@ export default function Video() {
       <button onClick={handleVideo2} className="bg-red-700 text-white block">
         video 2
       </button>
+      {isLoaded && (
+        <>
+          <div>{duration}</div>
+          <div>{currentTime}</div>
+          <div className={"w-[400px]"}>
+            <InputRange
+              value={currentTime}
+              max={duration}
+              min={0}
+              onInput={(e) => handleSeekTo(e.value)}
+            />
+          </div>
+        </>
+      )}
     </>
   );
 }
